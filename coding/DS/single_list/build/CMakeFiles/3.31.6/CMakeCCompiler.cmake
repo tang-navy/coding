e64c6a523c770d721e62b9fffc111d1bc3a53876
thj <1794758142@qq.com>
2025-03-22 12:39:12 +0800

    修改

--- coding/DS/single_list/build/CMakeFiles/3.31.6/CMakeCCompiler.cmake
+++ coding/DS/single_list/build/CMakeFiles/3.31.6/CMakeCCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_C_COMPILER "C:/data/appdata/mingw64/bin/cc.exe")
+set(CMAKE_C_COMPILER "C:/data/appdata/mingw64/bin/gcc.exe")
 set(CMAKE_C_COMPILER_ARG1 "")
 set(CMAKE_C_COMPILER_ID "GNU")
 set(CMAKE_C_COMPILER_VERSION "14.2.0")
